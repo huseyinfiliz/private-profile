4a08790c13de0739a00affa22fb38c1003fae64e
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2025-02-05 19:48:36 +0300

    Create extend.php

---
+++ extend.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace huseyinfiliz\PrivateProfile;
+
+use Flarum\Extend;
+use Flarum\User\User;
+use Flarum\Api\Serializer\UserSerializer;
+use Flarum\Api\Middleware\ThrottleApi;
+use huseyinfiliz\PrivateProfile\Middleware\CheckUserPrivateStatus;
+
+return [
+    (new Extend\Frontend('forum'))
+        ->js(__DIR__.'/js/dist/forum.js')
+        ->css(__DIR__.'/less/forum.less'),
+
+    (new Extend\Locales(__DIR__.'/locale')),
+
+    (new Extend\Middleware('api'))
+        ->insertBefore(ThrottleApi::class, CheckUserPrivateStatus::class),
+
+    (new Extend\User())
+        ->registerPreference('PrivateProfile', 'boolval', false),
+
+    (new Extend\ApiSerializer(UserSerializer::class))
+        ->attribute('privateProfile', function (UserSerializer $serializer, User $user) {
+            return $user->getPreference('PrivateProfile', false);
+        }),
+];
