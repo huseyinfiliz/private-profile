37b8ec70d4c04b4fa358ed400a4015762777eac4
Hüseyin Filiz <37220000+huseyinfiliz@users.noreply.github.com>
2025-02-05 20:35:33 +0300

    Update extend.php

--- extend.php
+++ extend.php
@@ -10,8 +10,7 @@ use huseyinfiliz\PrivateProfile\Middleware\CheckUserPrivateStatus;
 
 return [
     (new Extend\Frontend('forum'))
-        ->js(__DIR__.'/js/dist/forum.js')
-        ->css(__DIR__.'/less/forum.less'),
+        ->js(__DIR__.'/js/dist/forum.js'),
 
     (new Extend\Locales(__DIR__.'/locale')),
 
